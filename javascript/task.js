91303b10cbe990b79859cd7ec3c31479417b93d3
cosmin <cosminmorari99@yahoo.com>
2022-02-22 13:11:17 -0800

    tasks

--- javascript/task.js
+++ javascript/task.js
@@ -1,49 +1,47 @@
-//let describeTask = document.getElementById("describe"); //input
-let submit = document.getElementById("submitBtn");      // submit button
-let clear = document.getElementById("clearList");       //clear button
-let messageElement = document.getElementById("message");       //message text validation
+let submit = document.getElementById("submitBtn"); // submit button
+let clear = document.getElementById("clearList"); //clear button
+let messageElement = document.getElementById("message"); //message text validation
 let paragraphElement = document.getElementById("paragraph");
 
-submit.addEventListener('click', addTask)
-paragraphElement.addEventListener('click', handleTaskClick)
-clear.addEventListener('click', e =>{
-    paragraphElement.innerHTML= "";
-    displayMessage("astazi nu ai nimic");
-})
+submit.addEventListener("click", addTask);
+paragraphElement.addEventListener("click", handleTaskClick);
+clear.addEventListener("click", (e) => {
+  paragraphElement.innerHTML = "";
+  displayMessage("astazi nu ai nimic");
+});
 
-
-
-function handleTaskClick(event){
-    const style = event.target.style;
-    if(!style.textDecoration){
-        style.textDecoration = "line-through"
-}   else{
+function handleTaskClick(event) {
+  const style = event.target.style;
+  if (!style.textDecoration) {
+    style.textDecoration = "line-through";
+  } else {
     style.textDecoration = "";
+  }
 }
-}
-
 
 displayMessage("astazi nu ai nimic");
 
-function addTask(){
-    let describeTask = document.getElementById("describe");
-    if(inputValid(describeTask.value)){
-        paragraphElement.innerHTML+= '<li class="totul">' + describeTask.value + '</li>';
-        describeTask.value="";
-        messageElement.style.visibility = "hidden";
-}   else{
+function addTask() {
+  let describeTask = document.getElementById("describe");
+  if (inputValid(describeTask.value)) {
+    paragraphElement.innerHTML +=
+      '<li class="totul">' + describeTask.value + "</li>";
+    describeTask.value = "";
+    messageElement.style.visibility = "hidden";
+  } else {
     displayMessage("Te rog introdu un mesaj");
-}
+  }
 }
 
-function inputValid(input){
-    if(input){
-        return true;
-    } else{
-        return false;
-}}
+function inputValid(input) {
+  if (input) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-function displayMessage(message){
-    messageElement.innerHTML=message;
-    messageElement.style.visibility = "visible";
+function displayMessage(message) {
+  messageElement.innerHTML = message;
+  messageElement.style.visibility = "visible";
 }
