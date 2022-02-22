f847493dfdd97f7036f296455ad6ff0367a1fc49
cosmin <cosminmorari99@yahoo.com>
2022-02-21 18:25:45 -0800

    changes

--- javascript/task.js
+++ javascript/task.js
@@ -1,1 +1,14 @@
-let describeTask = document.getElementById("describe");
+let describeTask = document.getElementById("describe"); //input
+let submit = document.getElementById("submitBtn");      // submit button
+let clear = document.getElementById("clearList");       //clear button
+let paragraph = document.getElementById("paragraph");
+
+
+submit.addEventListener('click', e=> {
+   paragraph.innerHTML+=describeTask.value;
+})
+
+clear.addEventListener('click', e => {
+    paragraph.innerHTML ="";
+})
+
