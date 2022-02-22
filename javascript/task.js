68e108de0480b9d0a871aefcdc4aadbb9a327757
cosmin <cosminmorari99@yahoo.com>
2022-02-22 13:08:29 -0800

    JS functionalities

--- javascript/task.js
+++ javascript/task.js
@@ -1,14 +1,49 @@
-let describeTask = document.getElementById("describe"); //input
+//let describeTask = document.getElementById("describe"); //input
 let submit = document.getElementById("submitBtn");      // submit button
 let clear = document.getElementById("clearList");       //clear button
-let paragraph = document.getElementById("paragraph");
+let messageElement = document.getElementById("message");       //message text validation
+let paragraphElement = document.getElementById("paragraph");
 
-
-submit.addEventListener('click', e=> {
-   paragraph.innerHTML+=describeTask.value;
+submit.addEventListener('click', addTask)
+paragraphElement.addEventListener('click', handleTaskClick)
+clear.addEventListener('click', e =>{
+    paragraphElement.innerHTML= "";
+    displayMessage("astazi nu ai nimic");
 })
 
-clear.addEventListener('click', e => {
-    paragraph.innerHTML ="";
-})
 
+
+function handleTaskClick(event){
+    const style = event.target.style;
+    if(!style.textDecoration){
+        style.textDecoration = "line-through"
+}   else{
+    style.textDecoration = "";
+}
+}
+
+
+displayMessage("astazi nu ai nimic");
+
+function addTask(){
+    let describeTask = document.getElementById("describe");
+    if(inputValid(describeTask.value)){
+        paragraphElement.innerHTML+= '<li class="totul">' + describeTask.value + '</li>';
+        describeTask.value="";
+        messageElement.style.visibility = "hidden";
+}   else{
+    displayMessage("Te rog introdu un mesaj");
+}
+}
+
+function inputValid(input){
+    if(input){
+        return true;
+    } else{
+        return false;
+}}
+
+function displayMessage(message){
+    messageElement.innerHTML=message;
+    messageElement.style.visibility = "visible";
+}
